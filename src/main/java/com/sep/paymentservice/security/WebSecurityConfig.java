ed22771a438b6c5b35abdb114a229dbe73025aff
Pavle Jankovic <pavle.gp@gmail.com>
2019-11-28 10:50:10 +0100

    Init

---
+++ src/main/java/com/sep/paymentservice/security/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.sep.paymentservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel().anyRequest().requiresSecure()
+                .and().authorizeRequests().antMatchers("/**").permitAll();
+
+        http.csrf().disable();
+    }
+
+}
