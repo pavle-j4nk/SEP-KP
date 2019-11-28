46fedf35f3b0389aefca2b745ea1471cb916b715
Pavle Jankovic <pavle.gp@gmail.com>
2019-11-28 10:56:39 +0100

    Allow insecure connections from forwarded requests

--- src/main/java/com/sep/paymentservice/security/WebSecurityConfig.java
+++ src/main/java/com/sep/paymentservice/security/WebSecurityConfig.java
@@ -3,13 +3,16 @@ package com.sep.paymentservice.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requiresChannel().anyRequest().requiresSecure()
+        RequestMatcher notForwarded =  r -> r.getHeader("X-Forwarded-Proto") != null;
+
+        http.requiresChannel().requestMatchers(notForwarded).requiresSecure()
                 .and().authorizeRequests().antMatchers("/**").permitAll();
 
         http.csrf().disable();
