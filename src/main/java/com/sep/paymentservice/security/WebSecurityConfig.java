ade1167435a8d6337eedd681225e7ecb61c286d0
Pavle Jankovic <pavle.gp@gmail.com>
2019-11-29 17:38:40 +0100

    Java 11

--- src/main/java/com/sep/paymentservice/security/WebSecurityConfig.java
+++ src/main/java/com/sep/paymentservice/security/WebSecurityConfig.java
@@ -10,8 +10,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requiresChannel()
-                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-                .requiresSecure();
+        RequestMatcher notForwarded =  r -> r.getHeader("X-Forwarded-Proto") != null;
+
+        http.requiresChannel().requestMatchers(notForwarded).requiresSecure()
+                .and().authorizeRequests().antMatchers("/**").permitAll();
+
+        http.csrf().disable();
     }
+
 }
